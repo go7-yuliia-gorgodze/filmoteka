6b8975657802725c4d12333eaee164743f985eef
Mikhail Borodai <boroday.mihail@gmail.com>
2021-01-28 00:52:36 +0200

    перемещает функции в нужный паршал

--- src/js/1initialHomePage.js
+++ src/js/1initialHomePage.js
@@ -97,30 +97,3 @@ function createCard(imgPath, movieTitle, movieId, date) {
 
   return movieItem.outerHTML;
 }
-
-movieGallery.addEventListener('click', event => {
-  let id = event.target.dataset.id;
-  activateDetailsPage(id);
-});
-
-function activateDetailsPage(id) {
-  homePage.classList.add('is-hidden');
-  searchField.classList.remove('is-hidden');
-
-  let selectedMovie = renderedMovies.find(movie => movie.id === Number(id));
-
-  console.log(selectedMovie);
-
-  openMovieDetails(selectedMovie);
-}
-
-function openMovieDetails(selectedMovie) {
-  detailsPreviewImg.setAttribute(
-    'src',
-    `https://image.tmdb.org/t/p/w500/${selectedMovie.poster_path}`,
-  );
-  detailsTitle.textContent = selectedMovie.title;
-  detailsDescription.textContent = selectedMovie.overview;
-  detailsPopularuty.innerHTML = selectedMovie.popularity;
-  detailsOriginalTitle.innerHTML = selectedMovie.original_title;
-}
