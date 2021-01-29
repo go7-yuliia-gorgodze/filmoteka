dcfa1403e5ae335bba72cc290042f0968ee1517c
Yuliia-Gorgodze <taragorgodze@gmail.com>
2021-01-29 13:32:13 +0200

    cursor

---
+++ src/js/cursor.js
@@ -0,0 +1,12 @@
+// 'use strict';
+// const detailsButton = document.querySelector('.details-button-list');
+// const button = document.querySelector('.details-button');
+
+// document.addEventListener('mouseover', e => {
+//   if (e.target.nodeName === 'BUTTON') {
+//     console.log('Button');
+//   }
+// });
+const cursor = document.getElementById('cursor');
+const follower = document.getElementById('aura');
+const links = document.getElementById('a');
