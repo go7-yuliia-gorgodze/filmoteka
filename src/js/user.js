ef94504b6ebc429c62ac48723581766f40502c4c
Osipov Sergey <smozoluk@gmail.com>
2021-01-30 17:30:53 +0200

    add logIn logOut

--- src/js/user.js
+++ src/js/user.js
@@ -1,4 +1,6 @@
 const registrationButton = document.querySelector('.button_registration');
+const logInButton = document.querySelector('.button_logIn');
+const signOutButton = document.querySelector('.button_signOut');
 const registrationMail = document.querySelector('.registration .mail');
 const registrationPass = document.querySelector('.registration .pass');
 const logInMail = document.querySelector('.logIn .mail');
@@ -6,12 +8,26 @@ const logInPass = document.querySelector('.logIn .pass');
 
 registrationButton.addEventListener('click', event => {
   event.preventDefault();
+  createUser(registrationMail.value, registrationPass.value);
+  registrationMail.value = '';
+  registrationPass.value = '';
+});
+logInButton.addEventListener('click', event => {
+  event.preventDefault();
+  logInUser(logInMail.value, logInPass.value);
+  logInMail.value = '';
+  logInPass.value = '';
+});
+
+signOutButton.addEventListener('click', event => {
+  event.preventDefault();
+  signOut();
+});
+
+function createUser(email, password) {
   firebase
     .auth()
-    .createUserWithEmailAndPassword(
-      registrationMail.value,
-      registrationPass.value,
-    )
+    .createUserWithEmailAndPassword(email, password)
     .then(userCredential => {
       // Signed in
       var user = userCredential.user;
@@ -21,7 +37,37 @@ registrationButton.addEventListener('click', event => {
     .catch(error => {
       var errorCode = error.code;
       var errorMessage = error.message;
-      console.log(errorCode, errorMessage);
+      console.log('register error:', errorCode, errorMessage);
       // ..
     });
-});
+}
+
+function logInUser(email, password) {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(userCredential => {
+      // Signed in
+      var user = userCredential.user;
+      console.log('logIn user:', user);
+      // ...
+    })
+    .catch(error => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log('login error:', errorCode, errorMessage);
+    });
+}
+function signOut() {
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      // Sign-out successful.
+      console.log('Sign-out successful');
+    })
+    .catch(error => {
+      console.log('signOut error:', error);
+      // An error happened.
+    });
+}
