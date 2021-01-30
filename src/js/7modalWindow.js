48ef60c012d1e92285d042db0f8c4df9703f0ee6
Dm3583 <pankovdm@gmail.com>
2021-01-29 17:32:13 -0800

    update

--- src/js/7modalWindow.js
+++ src/js/7modalWindow.js
@@ -8,36 +8,59 @@ const header = document.querySelector('header');
 const html = document.documentElement;
 let scrollPosition = window.pageYOffset;
 
-console.log(html)
+function focusCatcher() {
+    const focusableElements = html.querySelectorAll(FOCUSABLE_SELECTORS);
+    focusableElements.forEach(el => el.setAttribute('tabindex', '-1'));
+};
 
-// function focusCatcher() {
-//     modal.querySelector(FOCUSABLE_SELECTORS).focus();
-//     const focusableElements = main.querySelectorAll(FOCUSABLE_SELECTORS);
-//     focusableElements.forEach(el => el.setAttribute('tabindex', '-1'));
-// }
+function focusSet(element) {
+    const focusableElements = element.querySelectorAll(FOCUSABLE_SELECTORS);
+    focusableElements.forEach(el => el.removeAttribute('tabindex'));
+};
+
+function trapScreenReaderFocus() { 
+    modal.removeAttribute('aria-hidden');
+    main.setAttribute('aria-hidden', 'true');
+    header.setAttribute('aria-hidden', 'true');
+};
+
+function untrapScreenReaderFocus() {
+    modal.setAttribute('aria-hidden', 'true');
+    main.removeAttribute('aria-hidden');
+    header.removeAttribute('aria-hidden');
+};
+ 
+function scrollPositionOnOpen() { 
+        scrollPosition = window.pageYOffset;
+    html.style.top = -scrollPosition + "px";
+}
+
+function scrollPositionOnClose() { 
+        html.classList.remove("modal__opened");
+    window.scrollTo(0, scrollPosition);
+    html.style.top = "";
+};
 
 function openModalWindow() {
     // show the modal
     modal.classList.add('modal--active');
-    closeModalBtn.addEventListener('click', closeModalWindow);
-    // Focus the first element within the modal. Make sure the element is visible and doesnt have focus disabled (tabindex=-1);
+    closeModalBtn.addEventListener('click', closeModalWindow);  
+    // Trap the tab focus by disable tabbing on all elements outside of your modal.  Because the modal is a sibling of main, this is easier. Make sure to check if the element is visible, or already has a tabindex so you can restore it when you untrap.
     
-
-    // Trap the tab focus by disable tabbing on all elements outside of your modal.  Because the modal is a sibling of main, this is easier. Make sure to check if the element is visible, or already has a tabindex so you can restore it when you untrap.    
-    modal.querySelector(FOCUSABLE_SELECTORS).focus();
-    const focusableElementsMain = main.querySelectorAll(FOCUSABLE_SELECTORS);
-    focusableElementsMain.forEach(el => el.setAttribute('tabindex', '-1'));
-    const focusableElementsHeader = header.querySelectorAll(FOCUSABLE_SELECTORS);
-    focusableElementsHeader.forEach(el => el.setAttribute('tabindex', '-1'));
-
-    scrollPosition = window.pageYOffset;
-    html.style.top = -scrollPosition + "px";
-    html.classList.add("modal__opened");
+        // Focus the first element within the modal. Make sure the element is visible and doesnt have focus disabled (tabindex=-1);
+    focusCatcher();
+    console.log(modal.querySelector(FOCUSABLE_SELECTORS));
+    focusSet(modal);
     
+    // const focusableElementsMain = main.querySelectorAll(FOCUSABLE_SELECTORS);
+    // focusableElementsMain.forEach(el => el.setAttribute('tabindex', '-1'));
+    // const focusableElementsHeader = header.querySelectorAll(FOCUSABLE_SELECTORS);
+    // focusableElementsHeader.forEach(el => el.setAttribute('tabindex', '-1'));
+
+    scrollPositionOnOpen();
+    html.classList.add("modal__opened"); 
     // Trap the screen reader focus as well with aria roles. This is much easier as our main and modal elements are siblings, otherwise you'd have to set aria-hidden on every screen reader focusable element not in the modal.
-    modal.removeAttribute('aria-hidden');
-    main.setAttribute('aria-hidden', 'true');
-    header.setAttribute('aria-hidden', 'true');
+    trapScreenReaderFocus();
 };
 
 function closeModalWindow() {
@@ -51,21 +74,12 @@ function closeModalWindow() {
 function transitionClose() { 
     modal.classList.remove("modal--moved");
     modal.removeEventListener("transitionend", transitionClose);
-    const focusableElements = main.querySelectorAll(FOCUSABLE_SELECTORS);
-    focusableElements.forEach(el => el.removeAttribute('tabindex'));
-
-    html.classList.remove("modal__opened");
-    window.scrollTo(0, scrollPosition);
-    html.style.top = "";
-
+    focusSet(html);
+    scrollPositionOnClose();
     // Untrap screen reader focus
-    modal.setAttribute('aria-hidden', 'true');
-    main.removeAttribute('aria-hidden');
-    header.removeAttribute('aria-hidden');
-
+    untrapScreenReaderFocus();
     // restore focus to the triggering element
-    openModalBtn.focus();
-       
+    openModalBtn.focus();      
 };
 
 openModalBtn.addEventListener('click', openModalWindow);
